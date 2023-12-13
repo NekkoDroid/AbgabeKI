79f718ca11c19890c26f0e41981a407ffab4136e
Michael Ferrari <nekkodroid404@gmail.com>
2023-12-13 16:36:37 +0100

    Attempt to understand the tensorflow tutorial
    REF: https://www.tensorflow.org/tutorials/quickstart/beginner#build_a_machine_learning_model

--- src/main.py
+++ src/main.py
@@ -1,4 +1,47 @@
 import tensorflow as tf
 
 if __name__ == '__main__':
-	print('Tensorflow version: ', tf.__version__)
+	(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
+
+	"""
+	The images in the MNIST dataset have pixel values that range from [0, 255]
+	Reduce the pixel values to [0.0, 1.0) floating point numbers to give to the model
+	"""
+	train_images = train_images / 255.0
+	test_images = test_images / 255.0
+
+	"""
+	Sequential network models feed the results from one layer directly to the following layer
+
+	Layers:
+		1 -> Flatten the image from a 28x28 2d-matrix to a 784 1d-matrix
+		2 -> Reduce the input to a fully connected 128 1d-matrix
+		3 -> Drop 20% of the input to prevent over-fitting to the training dataset
+		4 -> Reduce the output to 10 values mapping to the 10 labels of the dataset
+	"""
+	model = tf.keras.models.Sequential([
+		tf.keras.layers.Flatten(input_shape=train_images.shape[1:]),
+		tf.keras.layers.Dense(128, activation=tf.keras.activations.relu),
+		tf.keras.layers.Dropout(0.2),
+		tf.keras.layers.Dense(10, activation=tf.keras.activations.softmax),
+	])
+
+	"""
+	Compile the model with certain settings during the training:
+	optimizer: <I-couldn't-figure-out-what-the-fuck-this-is-used-for>
+	loss: Calculates the loss (how wrong) of the model during training
+	metrics: Used to decides how the results should be evaluated (relates to 'loss')
+	"""
+	model.compile(
+		optimizer=tf.keras.optimizers.Adam(),
+		loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+		metrics=[
+			tf.keras.metrics.SparseCategoricalAccuracy(),
+		]
+	)
+
+	"""Train the model on the training dataset"""
+	model.fit(train_images, train_labels, epochs=5)
+
+	"""Evaluate the trained model on the test dataset"""
+	model.evaluate(test_images, test_labels, verbose=2)
